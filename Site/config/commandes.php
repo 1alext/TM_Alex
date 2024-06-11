92d13e181e395cd278aa536e5c16ef48ab5bf96d
Alex Trieu <alex.trieu@edu.gbjb.ch>
2024-06-11 15:31:16 +0200

    php ajout produit

--- Site/config/commandes.php
+++ Site/config/commandes.php
@@ -1,11 +1,22 @@
 <?php
 
-    function ajouter($nom, $image, $prix, $description)
+    function ajouter($image, $nom, $prix, $description)
     {
-        if(require("connexion.php")) #(si une connexion a lieu avec la base de donées, exécute le code )
-        {
-            $req = $access->prepare("INSERT INTO produits (image, nom, prix, description) VALUES ()")
-        }                                
-    }
+        if(require("connexion.php")) #si une connexion a lieu avec la base de donées, exécute le code )
+        { #prépare l'insertion avant executer
+            $req = $access->prepare("INSERT INTO produits (image, nom, prix, description) VALUES ($image, $nom, $prix, $description)");
+        
+            $req->execute(array($image, $nom, $prix, $description));
 
+            $req->closeCursor();
+        }   
+    }
+function afficher()
+{
+    if(require("connexion.php"))
+    {
+        $req=$access->prepare("SELECT * FROM produits ORDER BY id DESC")
+        $req->execute();
+    }
+}
 ?>
