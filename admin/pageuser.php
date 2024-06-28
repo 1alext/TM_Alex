6ef8dc7a0ea91222e3b492496816ec43fb08e1ba
Alex Trieu <alex.trieu@edu.gbjb.ch>
2024-06-28 21:33:58 +0200

    pageuser

---
+++ admin/pageuser.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>page user</title>
+    
+    <link rel="stylesheet" href="Lien.css"/>
+</head>
+<body>
+<div class="admin.container">
+
+    <div class="content">
+        <h3>Salut, l'utilisateur</h3>
+        <h1>Bienvenue</h1>
+        <p>A la page de l'utilisateur</p>
+        <a href="logout form.php" class="btn">logout</a>
+    </div>
+</div>    
+</body>
+</html> 
