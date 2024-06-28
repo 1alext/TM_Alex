0bbb2d4a058d4373bca8034fb53ced7c39444045
Alex Trieu <alex.trieu@edu.gbjb.ch>
2024-06-28 21:41:09 +0200

    corriger erreur de syntaxe pour définir les classe css

--- pageadmin.php
+++ pageadmin.php
@@ -3,19 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>page admin</title>
-    
+    <title>Page Admin</title>
     <link rel="stylesheet" href="Lien.css"/>
 </head>
 <body>
-<div class="admin.container">
-
+<div class="admin-container">
     <div class="content">
         <h3>Salut, l'admin</h3>
         <h1>Bienvenue</h1>
         <p>A la page administrateur</p>
-        <a href="logout form.php" class="btn">logout</a>
+        <a href="logout form.php" class="btn">Logout</a>
     </div>
 </div>    
 </body>
-</html> 
+</html>
