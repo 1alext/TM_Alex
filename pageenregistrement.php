8f0670d4f26e054c101e2e33c9aa0bf65f6d7142
Alex Trieu <alex.trieu@edu.gbjb.ch>
2024-06-29 17:47:26 +0200

    ajout config pour lier bd

--- pageenregistrement.php
+++ pageenregistrement.php
@@ -10,9 +10,10 @@
 <div class="enregistrement-container">
     <form class="enregistrement" action="" method="post">    
         <h3>S'enregistrer</h3>
-        <input type="email" name="email" required placeholder="Entrer votre email">
-        <input type="password" name="password" required placeholder="Entrer votre mot de passe">
-        <input type="submit" name="submit" value="Se connecter" class="enregistrement-btn">
+        <input type="email" name="email" required placeholder="Adresse e-mail">
+        <input type="password" name="password" required placeholder="Mot de passe">
+        <input type="password" name="cpassword" required placeholder="Confirmer votre mot de passe">
+        <input type="submit" name="submit" value="S'enregistrer" class="enregistrement-btn">
         <p>Déjà un compte ? <a href="pagelogin.php">Se connecter</a></p>
     </form>
 </div>
---
+++ pagelogin.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page se connecter</title>
+    <link rel="stylesheet" href="Lien.css"/>
+</head>
+<body>
+<div class="enregistrement-container">
+    <form class="enregistrement" action="" method="post">    
+        <h3>Se connecter</h3>
+        <input type="email" name="email" required placeholder="Adresse e-mail">
+        <input type="password" name="password" required placeholder="Mot de passe">
+        <input type="submit" name="submit" value="Se connecter" class="enregistrement-btn">
+        <p>Pas de compte ? <a href="pageenregistrement.php">S'enregistrer</a></p>
+    </form>
+</div>
+</body>
+</html>
