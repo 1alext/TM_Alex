2a730759583f08d28f126d71c3c3699bff3e7c1c
Alex Trieu <alex.trieu@edu.gbjb.ch>
2024-06-29 14:05:21 +0200

    ajout page enregistrement

--- pageuser.php
+++ pageuser.php
@@ -4,12 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>page user</title>
-    
     <link rel="stylesheet" href="Lien.css"/>
 </head>
 <body>
-<div class="admin.container">
-
+<div class="admin-container">
     <div class="content">
         <h3>Salut, l'utilisateur</h3>
         <h1>Bienvenue</h1>
