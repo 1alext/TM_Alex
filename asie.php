7e1ca1b4397ccf78a4cce295cad84c2f0a9e31e7
Alex Trieu <alex.trieu@edu.gbjb.ch>
2024-10-02 02:22:35 +0200

    ajout section dans les différentes pages

--- asie.php
+++ asie.php
@@ -73,4 +73,120 @@ $produitsAsie = $query->fetchAll(PDO::FETCH_ASSOC);
         <p>Aucun produit trouvé.</p>
     <?php endif; ?>
 </body>
+
+<!-- Bas page catégorie -->
+<section class="continent-section">
+    <h2>Sélectionnez un continent pour trouver votre maillot</h2>
+    <div class="continent-grid">
+        <a href="europe.php" class="continent-card">
+            <img src="asset/europecart.jpg" alt="Europe">
+            <p>Europe</p>
+        </a>
+        
+        <a href="ameriquesud.php" class="continent-card">
+            <img src="asset/ameriquesud.jpg" alt="Amerique du Sud">
+            <p>Amérique du Sud</p>
+        </a>
+    
+        <a href="ameriquenord.php" class="continent-card">
+            <img src="asset/ameriquenord.jpg" alt="Amerique du Nord">
+            <p>Amérique du Nord</p>
+        </a>
+    
+        <a href="afrique.php" class="continent-card">
+            <img src="asset/afrique.jpg" alt="Afrique">
+            <p>Afrique</p>
+        </a>
+    </div>
+</section>
+
+<!--A propos-->
+
+<section id="about">
+    <div class="contenubas">
+        <!-- Section A propos -->
+        <div class="about-text">
+            <h3>À propos</h3>
+            <p>Hardvest est votre boutique en ligne dédiée aux maillots de football, des dernières saisons aux classiques incontournables. Nous sélectionnons des produits authentiques et de qualité pour satisfaire les passionnés et les collectionneurs.
+            Notre objectif est de vous offrir une expérience d'achat unique avec des maillots qui combinent confort, style et performance. Explorez notre sélection et rejoignez notre communauté de fans de football.</p>
+            <p>Merci de choisir Hardvest !</p>
+        </div>
+        <div class="social">
+            <i class='bx bxl-instagram'></i>
+            <i class='bx bxl-tiktok' ></i>
+            <i class='bx bxl-pinterest' ></i>
+        </div>
+        <!-- Section des icônes -->
+        <div class="icon-section">
+            <div class="icon-circle"><i class='bx bxs-truck'></i></div>
+            <div class="icon-circle"><i class='bx bxs-timer'></i></div>
+                <div class="icon-circle"><i class='bx bxs-t-shirt'></i></div>
+        </div>
+    </div>
+</section>
+
+<section id="contact">
+    <div class="container">
+        <div>
+            <h6>Une question ?</h6>
+            <h3>Contactez-nous</h3>
+        </div>
+        <form id="contactForm" action="#contact" method="post">
+            <input type="email" name="email" placeholder="Entrer votre email" required>
+            <input type="text" name="subject" placeholder="Entrer le sujet" required>
+            <textarea name="message" placeholder="Entrer votre message" required></textarea>
+            <button type="submit" name="submit">Envoyer</button>
+        </form>
+        <div id="response">
+
+    <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $sujet = $_POST['subject'];
+    $message = $_POST['message'];
+
+    //Liste des mails autorisés
+    $allowedDomains = ['gmail.com', 'yahoo.com', 'outlook.com', 'gbjb.ch', 'icloud.com', 'hotmail.com']; 
+
+    //Valider l'adresse email avec FILTER_VALIDATE_EMAIL qui est un filtre spécifique
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo '<div style="color: black; margin-top: 20px; font-size: 18px;">Adresse email invalide.</div>';
+
+    } else {
+        //Extraire le domaine de l'email donc garde seulement après le @ pour en suite vérifier avec la liste
+        $domain = substr(strrchr($email, "@"), 1);
+
+        //Vérifier si le domaine est dans la liste des domaines autorisés
+        if (!in_array($domain, $allowedDomains)) {
+            echo '<div style="color: black; margin-top: 20px; font-size: 18px;">Adresse email invalide.</div>';
+
+        } else {
+            try {
+                //Connexion à la base de données
+                $access = new PDO("mysql:host=localhost;dbname=hardvest;charset=utf8", "root", "");
+                $access->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                //Exécute la requête d'insertion
+                $sql = "INSERT INTO contacts (email, sujet, message) VALUES (:email, :sujet, :message)";
+                $stmt = $access->prepare($sql);
+                $stmt->execute([
+                    ':email' => $email,
+                    ':sujet' => $sujet,
+                    ':message' => $message
+                ]);
+
+                echo '<div style="color: black; margin-top: 20px; font-size: 18px;">Message envoyé avec succès !</div>';
+
+            } catch (Exception $e) {
+                echo "Erreur : " . $e->getMessage();
+            }
+        }
+    }
+    //Exit sert à éviter d'autres execution de codes
+    exit();
+}
+?>
+    </div>
+</section>
+
 </html>
